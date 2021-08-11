3be6561da5286a9bf7f66fc5aecfad3c4cc0bd1f
digant73 <diguardo@tiscali.it>
2021-08-11 13:35:08 +0200

    update

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1880,7 +1880,11 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
+<<<<<<< HEAD
 #define LIN_ADVANCE                                                                      // DIGA-Tech:
+=======
+#define LIN_ADVANCE                                        // DIGA-Tech:
+>>>>>>> d4377c5c3a (update)
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0.13    // Unit: mm compression per 1mm/s extruder speed         // DIGA-Tech:
